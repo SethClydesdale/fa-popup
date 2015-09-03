20965d5ca80c2ec4c3bbdb7a3f1ba68f16176229
Seth Clydesdale <sethc1995@gmail.com>
2015-09-03 06:52:13 -0400

    Create plugin_popup_preview.js

---
+++ plugin_popup_preview.js
@@ -0,0 +1,24 @@
+// quickly view previews by opening a popup window
+$(function() {
+  if (!window.FA.Popup || !document.post || !document.post.message || !document.post.mode) return;
+  
+  document.post.preview.onclick = function() {
+    var mode = document.post.mode.value;
+    
+    if ($.sceditor) document.post.message.value = $(document.post.message).sceditor('instance').val();
+    
+    $.post(mode == 'post' ? '/privmsg' : '/post', $(document.post).serialize() + '&preview=1', function(data) {
+      var preview = $(['.postbody', mode == 'post' ? '.post' : '#preview', '.main-content.topic', '#preview'][FA.Popup.forum.version], data)[0];
+      if (preview) {
+        FA.Popup.open(null, 'Preview', function(popup) {
+          var content = document.createElement('DIV');
+          content.className = 'fa_popup_preview';
+          content.appendChild(preview);
+          popup.appendChild(content);
+        });
+      }
+    });
+    
+    return false;
+  }
+});
